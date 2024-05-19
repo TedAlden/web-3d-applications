094528c331ff5c832b9a6227196fb5c78fd4df19
Ted <t.aldentempleman@gmail.com>
2024-05-19 23:39:42 +0100

    Lab 5 complete

---
+++ lab5/js/page-swap.js
@@ -0,0 +1,7 @@
+function swap(selected) {
+	document.getElementById('home').style.display = 'none';
+	document.getElementById('coke').style.display = 'none';
+	document.getElementById('sprite').style.display = 'none';
+	document.getElementById('pepper').style.display = 'none';
+	document.getElementById(selected).style.display = 'block';
+}

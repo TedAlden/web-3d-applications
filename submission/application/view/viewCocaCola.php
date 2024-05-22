e34f95914f2162924a76e252749135954952f0fe
Ted <t.aldentempleman@gmail.com>
2024-05-22 18:50:40 +0100

    Final Project - Changed about page

--- submission/application/view/viewCocaCola.php
+++ submission/application/view/viewCocaCola.php
@@ -303,7 +303,8 @@
             <!-- About page -->
             <div id="about" style="display: none;">
                 <h1>About this project</h1>
-                A 3D web application showcasing Coca Cola brands through 3D modelling using PHP, SQLite3, X3DOM, AJAX, JQuery.
+                <p>A 3D web application showcasing Coca Cola brands through 3D modelling using PHP, SQLite3, X3DOM, AJAX, JQuery.</p>
+                <p>This assignment is submitted as part requirement for the degree of MComp Computer Science at the University of Sussex. It is the product of my own labour except where indicated in the accompanying codebase. This assignment may be freely copied and distributed provided the source is acknowledged. I hereby give permission for a copy of this assignment to be loaned out to students in future years.</p>
             </div>
         </div>
 

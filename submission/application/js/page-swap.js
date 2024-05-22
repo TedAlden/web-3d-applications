21a910d936658e28c7493fbaebf08307d1b99d7e
Ted <t.aldentempleman@gmail.com>
2024-05-22 20:41:16 +0100

    Final Project - Refactor page-swap.js

--- submission/application/js/page-swap.js
+++ submission/application/js/page-swap.js
@@ -1,11 +1,3 @@
-// function swap(selected) {
-// 	document.getElementById('home').style.display = 'none';
-// 	document.getElementById('coke').style.display = 'none';
-// 	document.getElementById('sprite').style.display = 'none';
-// 	document.getElementById('pepper').style.display = 'none';
-// 	document.getElementById(selected).style.display = 'block';
-// }
-
 $(document).ready(function() {
     
     selectPage();

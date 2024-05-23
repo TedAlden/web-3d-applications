b1e372c8800d48f4a64b20bcc9f5153a76678fdd
Ted <t.aldentempleman@gmail.com>
2024-05-23 14:57:13 +0100

    Lab 9 - Removed
    This was example code from a previous years lab. Not from my year

--- lab9/SierraLeone_VM/javascript/createThumbnails.js
+++
@@ -1,4 +0,0 @@
-//Another JQuery document.ready function (the ready event) is used to run the jQuery $(.class).nailthumb() function
-$(document).ready(function() {
-   $('.nailthumb-container').nailthumb();
-});

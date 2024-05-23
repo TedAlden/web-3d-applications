f7d965c33758661f36eb35a02674cd4a438c6426
Ted <t.aldentempleman@gmail.com>
2024-05-23 12:00:30 +0100

    Final Project - Improved Flickr page styling and layout

--- submission/application/view/includes/flickr_page.php
+++ submission/application/view/includes/flickr_page.php
@@ -1,22 +1,34 @@
 <!-- Flickr webservice http://www.flickr.com/services/feeds/docs/photos_public/ -->
-<div style="height: 900px;">
-    <h1> Flickr Web Service</h1>
-    <h2> Search on Flickr Feed</h2>
-    <!-- Text input box -->
-    <div class="textInput">
-        <input type="text" value="Coca Cola Can" id="txt" />
+<div class="card text-left">
+    <div class="card-header description-header">
+        Flickr Search Service
     </div>
-    <!-- Search Button -->
-    <div class="btn">
-        <button onclick="loadImages()">Search</button>
-    </div>
-    <!-- Results -->
-    <div class="box">
-        <h1 id="title"></h1>
-        <h2 id="link"></h2>
-        <p id="description"></p>
-        <p id="modified"></p>
-        <p id="generator"></p>
-        <div id="images"></div>
+    <div class="card-body">
+        <div>
+            <h2> Search on Flickr Feed</h2>
+            <!-- Text input box -->
+            <div class="row">
+                <div class="col-sm-9">
+                    <div class="textInput">
+                        <input type="text" value="Coca Cola Can" id="txt" />
+                    </div>
+                </div>
+                <!-- Search Button -->
+                <div class="col-sm-3">
+                    <a href="#" class="btn btn-primary" onclick="loadImages();">Search</a>
+                </div>
+            </div>
+            <!-- Results -->
+            <div class="box">
+                <hr>
+                <h3 id="title"></h1>
+                <h4 id="link"></h2>
+                <p id="description"></p>
+                <p id="modified"></p>
+                <p id="generator"></p>
+                <hr>
+                <div id="images"></div>
+            </div>
+        </div>
     </div>
 </div>

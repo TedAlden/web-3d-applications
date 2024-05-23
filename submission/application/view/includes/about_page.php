1dd81b09e59915931bbf0f291cccbff2a5dc493c
Ted <t.aldentempleman@gmail.com>
2024-05-23 12:29:55 +0100

    Final Project - Added YouTube video to about page

--- submission/application/view/includes/about_page.php
+++ submission/application/view/includes/about_page.php
@@ -1,4 +1,8 @@
-<!-- About info -->
+<!-- About Coca Cola -->
+<h1>About Coca Cola</h1>
+<iframe width="420" height="315" src="https://www.youtube.com/embed/vHhaO56KmE4"></iframe>
+
+<!-- About this project -->
 <h1>About this project</h1>
 <p>A 3D web application showcasing Coca Cola brands through 3D modelling using PHP, SQLite3, X3DOM, AJAX, JQuery.</p>
 <p>This assignment is submitted as part requirement for the degree of MComp Computer Science at the University of Sussex. It is the product of my own labour except where indicated in the accompanying codebase. This assignment may be freely copied and distributed provided the source is acknowledged. I hereby give permission for a copy of this assignment to be loaned out to students in future years.</p>
